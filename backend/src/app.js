ee33965c86a194d2b1612108f3df6d7b557be309
instagram-user-identity <instragram.user.identity@gmail.com>
2025-11-13 16:30:44 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"*",
     credentials:true
 }))
 app.use('/auth',userRouter)
